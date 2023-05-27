758c23348b0abaf1baea9f489b51ce1a99ea0241
Sumanth <andavarapusumanth@gmail.com>
2023-05-27 19:09:19 +0530

    Deploying trail 3

--- UrlShortner/urls.py
+++ UrlShortner/urls.py
@@ -19,9 +19,12 @@ from django.urls import path, include
 
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shortner.urls')),
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
