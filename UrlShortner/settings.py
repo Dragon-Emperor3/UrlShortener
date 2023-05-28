410e95f67f15047c765ae5c0f3c0706a4a07488a
Sumanth <andavarapusumanth@gmail.com>
2023-05-28 08:17:13 +0530

    Deployment trail 5

--- UrlShortner/settings.py
+++ UrlShortner/settings.py
@@ -15,7 +15,7 @@ import os
 
 from decouple import config
 import dj_database_url
-import psycopg2
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS= [os.path.join(BASE_DIR, 'shortner/static')]
 
